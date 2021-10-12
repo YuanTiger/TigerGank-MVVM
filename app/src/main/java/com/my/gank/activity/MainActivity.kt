e2cf06946f0a1ff8f07b98c519db0232c6d8e734
孟远 <mengyuanzz@126.com>
2021-10-12 16:14:50 +0800

    自定义View使用LifeCycle

--- app/src/main/java/com/my/gank/activity/MainActivity.kt
+++ app/src/main/java/com/my/gank/activity/MainActivity.kt
@@ -3,12 +3,14 @@ package com.my.gank.activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Chronometer
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import com.my.gank.R
 import com.my.gank.base.lifecycle.ActivityLifecycleObserver
+import com.my.gank.test.lifecycle.MyChronometer
 import com.my.gank.utils.LogUtils
 
 /**
@@ -21,15 +23,17 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     val TAG = "MainActivity_";
 
+    var chronometer2: MyChronometer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         lifecycle.addObserver(object : ActivityLifecycleObserver() {})
-
         findViewById<TextView>(R.id.tv_test).setOnClickListener(this)
 
+        chronometer2 = findViewById(R.id.chronometer)
+        lifecycle.addObserver(chronometer2 ?: return)
 
     }
 
---
+++ app/src/main/java/com/my/gank/test/lifecycle/MyChronometer.java
@@ -0,0 +1,58 @@
+package com.my.gank.test.lifecycle;
+
+import android.content.Context;
+import android.os.SystemClock;
+import android.util.AttributeSet;
+import android.widget.Chronometer;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.DefaultLifecycleObserver;
+import androidx.lifecycle.LifecycleOwner;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Author: mengyuan
+ * Date  : 2021/10/12/4:05 下午
+ * E-Mail: mengyuanzz@126.com
+ * -----------
+ */
+public class MyChronometer extends Chronometer implements DefaultLifecycleObserver {
+
+    private long elapseTime;
+
+    public MyChronometer(Context context) {
+        super(context);
+    }
+
+    public MyChronometer(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyChronometer(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public MyChronometer(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+
+    @Override
+    public void onResume(@NonNull @NotNull LifecycleOwner owner) {
+
+        setBase(SystemClock.elapsedRealtime() - elapseTime);
+
+        start();
+
+    }
+
+
+    @Override
+    public void onPause(@NonNull @NotNull LifecycleOwner owner) {
+        elapseTime = SystemClock.elapsedRealtime() - getBase();
+        stop();
+    }
+
+
+}
