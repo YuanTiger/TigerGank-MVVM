9bdb2a31f8f315e1fc7f91ba03bd04d05198117f
孟远 <mengyuanzz@126.com>
2021-10-19 11:12:36 +0800

    navigation：了解NavigationUI

--- app/src/main/java/com/my/gank/activity/MainActivity.kt
+++ app/src/main/java/com/my/gank/activity/MainActivity.kt
@@ -16,6 +16,7 @@ import com.my.gank.test.databinding.three.DataBindingTestActivity_3
 import com.my.gank.test.databinding.two.DataBindingTestActivity_2
 import com.my.gank.test.lifecycle.MyChronometer
 import com.my.gank.test.navigation.one.Navigation_Activity_1
+import com.my.gank.test.navigation.two.Navigation_Activity_2
 import com.my.gank.test.room.one.RoomTestActivity_1
 import com.my.gank.test.room.two.RoomTestActivity_2
 
@@ -46,7 +47,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(view: View?) {
         when (view?.id) {
             R.id.tv_test -> {
-                val intent = Intent(this, Navigation_Activity_1::class.java)
+                val intent = Intent(this, Navigation_Activity_2::class.java)
                 startActivity(intent)
             }
             else -> {
---
+++ app/src/main/java/com/my/gank/test/navigation/two/Navigation_HomeFragment_2.java
@@ -0,0 +1,22 @@
+package com.my.gank.test.navigation.two;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.my.gank.R;
+
+/**
+ * Author: mengyuan
+ * Date  : 2021/10/19/10:44 上午
+ * E-Mail: mengyuanzz@126.com
+ * -----------
+ */
+public class Navigation_HomeFragment_2 extends Fragment {
+
+}
